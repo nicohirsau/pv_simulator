e0b48a736e78c432a995198dd2367311190948de
nicohirsau <preussens.nico@gmail.com>
2021-04-30 17:34:50 +0200

    Meter and PV Simulator are now split up into different files, since this aims to simulate two seperate systems communicating over the RabbitMQ

--- pvsimulator/pv_simulator.py
+++ pvsimulator/pv_simulator.py
@@ -1,1 +1,38 @@
-"""Main module."""
+from datetime import datetime
+import json
+import sys
+import time
+import random
+
+from pvsimulator.receiver import Receiver
+
+class PV_Simulator(Receiver):
+    def __init__(
+            self, 
+            host = 'localhost', 
+            username = 'guest', 
+            password = 'guest', 
+            queue_name = 'queue'
+        ):
+        super().__init__(host, username, password, queue_name)
+    
+    def _on_message_received_callback(self, method_body):
+        print(method_body)
+
+def simulate_photovoltaic_consumer():
+    pv = PV_Simulator(queue_name = 'pv_simulation')
+    pv.connect()
+    pv.start_consuming_blocking()
+
+def main(args=None):
+    try:
+        simulate_photovoltaic_consumer()
+    except KeyboardInterrupt:
+        print("Execution was cancelled by user!")
+        exit(0)
+    except Exception:
+        exit(0)
+
+if __name__ == "__main__":
+    sys.exit(main())
+
